9f3669aeebff53f8a37f63e3e9d60d45da3c6890
adrain.xue <adrain.xue@transfinder.com>
2018-04-04 21:27:13 +0800

    init project

--- TFARDirection/ViewController.swift
+++ TFARDirection/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     fileprivate var currentLocation:CLLocation?
     fileprivate var toLocation:CLLocation?
     fileprivate var toAnnotation:MKPointAnnotation?
+    fileprivate var routePolyline:MKPolyline!
     
     
     
@@ -66,26 +67,33 @@ extension ViewController {
         let configuration = ARWorldTrackingConfiguration()
         configuration.worldAlignment = .gravityAndHeading
         sceneView.session.run(configuration,  options: .resetTracking)
-        
+        self.addAnnotations()
     }
     
-    func addAnnotations(polyline:MKPolyline){
+    func addAnnotations(){
+        let polyline = self.routePolyline
         self.annotationManager?.originLocation = getUserLocation()
         self.annotationManager?.removeAllAnnotations()
         
-        let turfPolyline = Polyline(polyline.coordinates)
+        let turfPolyline = Polyline(polyline!.coordinates)
         let metersPerNode: CLLocationDistance = 2
         var annotationsToAdd:[Annotation] = []
-        
+        var totalShowCount = 20
+        var index = 0
         // Walk the route line and add a small AR node and map view annotation every metersPerNode
         for i in stride(from: metersPerNode, to: turfPolyline.distance() - metersPerNode, by: metersPerNode) {
             // Use Turf to find the coordinate of each incremented distance along the polyline
             if let nextCoordinate = turfPolyline.coordinateFromStart(distance: i) {
                 let interpolatedStepLocation = CLLocation(latitude: nextCoordinate.latitude, longitude: nextCoordinate.longitude)
-                
-                // Add an AR node
-                let annotation = Annotation(location: interpolatedStepLocation, calloutImage: nil)
-                annotationsToAdd.append(annotation)
+                if(self.distanceFromCurrent(to: interpolatedStepLocation) < 500){
+                    // Add an AR node
+                    let annotation = Annotation(location: interpolatedStepLocation, calloutImage: nil)
+                    annotation.index = index
+                    if index < totalShowCount {
+                        annotationsToAdd.append(annotation)
+                    }
+                    index = index + 1
+                }
             }
         }
         
@@ -98,8 +106,30 @@ extension ViewController:AnnotationManagerDelegate{
     func initAnnotationManager(){
         annotationManager = AnnotationManager(sceneView: sceneView)
         annotationManager!.delegate = self
-        
     }
+    
+    func node(for annotation: Annotation) -> SCNNode? {
+        let color = UIColor(red: 0, green:1, blue: 0, alpha: 1)
+        let minAlpha:CGFloat = 0.3
+        let maxIndex = 10
+        var firstColor = color.withAlphaComponent(minAlpha)
+        if let index = annotation.index {
+            if index < maxIndex {
+                firstColor = color.withAlphaComponent(1.0 - (1.0 - minAlpha)/10.0 * CGFloat(index))
+            }
+        }
+        return createSphereNode(with: 0.5, firstColor: firstColor, secondColor: UIColor.green)
+    }
+    
+    func createSphereNode(with radius: CGFloat, firstColor: UIColor, secondColor: UIColor) -> SCNNode {
+        let geometry = SCNSphere(radius: radius)
+        geometry.firstMaterial?.diffuse.contents = firstColor
+        
+        let sphereNode = SCNNode(geometry: geometry)
+        return sphereNode
+    }
+    
+    
 }
 
 extension ViewController: CLLocationManagerDelegate,MKMapViewDelegate{
@@ -147,16 +177,25 @@ extension ViewController: CLLocationManagerDelegate,MKMapViewDelegate{
         }
     }
     
+    func distanceFromCurrent(to:CLLocation) -> Double {
+        if let currentLocation = self.currentLocation {
+            let polyline = Polyline([currentLocation.coordinate,to.coordinate])
+            return  polyline.distance()
+        }
+        return 10000.0
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let nowCurrentLocation = self.getUserLocation()
         if let currentLocation = self.currentLocation {
             var polyline = Polyline([currentLocation.coordinate,nowCurrentLocation.coordinate])
-            if(self.mapView.overlays.count > 0 && polyline.distance() > 5) {
-                self.direction(to: self.toLocation!)
+            if(self.mapView.overlays.count > 0 && distanceFromCurrent(to:nowCurrentLocation) > 10) {
+                self.addAnnotations()
+                self.label.text = "renew"
             }
         }
         self.currentLocation = nowCurrentLocation
-        self.label.text = "\(nowCurrentLocation.coordinate.latitude) , \(nowCurrentLocation.coordinate.longitude)"
+        //self.label.text = "\(nowCurrentLocation.coordinate.latitude) , \(nowCurrentLocation.coordinate.longitude)"
         self.centerMapOnLocation(location: self.currentLocation!)
     }
     
@@ -207,7 +246,8 @@ extension ViewController: CLLocationManagerDelegate,MKMapViewDelegate{
                 return;
             }
             let myRoute = response.routes[0]
-            self.addAnnotations(polyline: myRoute.polyline)
+            self.routePolyline = myRoute.polyline
+            self.addAnnotations()
             self.mapView.add(myRoute.polyline)
         }
         
